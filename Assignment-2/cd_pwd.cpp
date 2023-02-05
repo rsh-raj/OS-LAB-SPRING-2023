85e72629bdeee99c15bd035d378189e7551c1b15
vikasb9749 <vb8987850@gmail.com>
2023-02-05 23:03:48 +0530

    Corrected \home in cd command and Edited readme.md

--- Assignment-2/cd_pwd.cpp
+++ Assignment-2/cd_pwd.cpp
@@ -60,7 +60,7 @@ int main(int argc, char *argv[])
         {
             if (cnt == 1)
             {
-                cd("/");
+                cd("/home");
             }
             else if (cnt == 2)
             {
